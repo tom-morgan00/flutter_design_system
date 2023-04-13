c0db7344950a2dae074255bfba21d0722c51c4e1
Tom <thomas.morgan1903@gmail.com>
2023-04-14 00:40:55 +0100

    added skeleton colors

---
+++ lib/design_system/foundations/colors/skeleton_colors.dart
@@ -0,0 +1,7 @@
+import 'color_palette.dart';
+import 'package:flutter/material.dart';
+
+class SkeletonColors {
+  static const Color skeleton = KColours.neutral200A;
+  static const Color subtle = KColours.neutral100A;
+}
