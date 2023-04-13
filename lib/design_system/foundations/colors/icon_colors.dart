5b5b112b442e7f8112dcb3c7fb9fe69defd09535
Tom <thomas.morgan1903@gmail.com>
2023-04-14 00:06:22 +0100

    added icon colors

---
+++ lib/design_system/foundations/colors/icon_colors.dart
@@ -0,0 +1,28 @@
+import 'color_palette.dart';
+import 'package:flutter/material.dart';
+
+class IconColors {
+  static const Color icon = KColours.neutral800;
+  static const Color disabled = KColours.neutral400A;
+  static const Color inverse = KColours.neutral0;
+  static const Color selected = KColours.blue700;
+  static const Color brand = KColours.blue700;
+  static const Color danger = KColours.red600;
+  static const Color warning = KColours.orange600;
+  static const Color warningInverse = KColours.neutral1000;
+  static const Color success = KColours.green600;
+  static const Color discovery = KColours.purple600;
+  static const Color information = KColours.blue600;
+  static const Color subtle = KColours.neutral700;
+
+  // colors
+  static const Color red = KColours.red600;
+  static const Color orange = KColours.orange600;
+  static const Color yellow = KColours.yellow600;
+  static const Color green = KColours.green600;
+  static const Color teal = KColours.teal600;
+  static const Color blue = KColours.blue600;
+  static const Color purple = KColours.purple600;
+  static const Color magenta = KColours.magenta600;
+  static const Color grey = KColours.neutral600;
+}
