fe30c8dfd8e28315b66892444645d82f42cab9d7
Tom <thomas.morgan1903@gmail.com>
2023-04-14 00:02:37 +0100

    added link colors

---
+++ lib/design_system/foundations/colors/link_colors.dart
@@ -0,0 +1,7 @@
+import 'color_palette.dart';
+import 'package:flutter/material.dart';
+
+class LinkColors {
+  static const Color link = KColours.blue700;
+  static const Color pressed = KColours.blue800;
+}
