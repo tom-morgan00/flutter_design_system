4fa81029e105affcb4b5252cdda481c11dfcdfbb
Tom <thomas.morgan1903@gmail.com>
2023-04-14 00:11:06 +0100

    added border colors

---
+++ lib/design_system/foundations/colors/border_colors.dart
@@ -0,0 +1,29 @@
+import 'color_palette.dart';
+import 'package:flutter/material.dart';
+
+class BorderColors {
+  static const Color border = KColours.neutral300A;
+  static const Color disabled = KColours.neutral200A;
+  static const Color focused = KColours.blue500;
+  static const Color input = KColours.neutral300A;
+  static const Color inverse = KColours.neutral0;
+  static const Color selected = KColours.blue700;
+  static const Color brand = KColours.blue700;
+  static const Color danger = KColours.red600;
+  static const Color warning = KColours.orange600;
+  static const Color success = KColours.green600;
+  static const Color discovery = KColours.purple600;
+  static const Color information = KColours.blue600;
+  static const Color bold = KColours.neutral600;
+
+  // colors
+  static const Color red = KColours.red600;
+  static const Color orange = KColours.orange600;
+  static const Color yellow = KColours.yellow600;
+  static const Color green = KColours.green600;
+  static const Color teal = KColours.teal600;
+  static const Color blue = KColours.blue600;
+  static const Color purple = KColours.purple600;
+  static const Color magenta = KColours.magenta600;
+  static const Color grey = KColours.neutral600;
+}
