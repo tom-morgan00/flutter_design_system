8401e9d33e45be1a297f32bb3b5cffc37db262ef
Tom <thomas.morgan1903@gmail.com>
2023-04-14 00:36:35 +0100

    added blanket colors

---
+++ lib/design_system/foundations/colors/blanket_colors.dart
@@ -0,0 +1,8 @@
+import 'color_palette.dart';
+import 'package:flutter/material.dart';
+
+class BlanketColors {
+  static const Color blanket = KColours.neutral500A;
+  static const Color selected = Color(0x14388BFF);
+  static const Color danger = Color(0x14EF5C48);
+}
