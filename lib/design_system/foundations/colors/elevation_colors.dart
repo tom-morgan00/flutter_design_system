364efb80518bedde2f5ad1e4ec6d4f2a3ecc7353
Tom <thomas.morgan1903@gmail.com>
2023-04-14 01:12:13 +0100

    added elevation colors

---
+++ lib/design_system/foundations/colors/elevation_colors.dart
@@ -0,0 +1,15 @@
+import 'color_palette.dart';
+import 'package:flutter/material.dart';
+
+class ElevationColors {
+  static const Color surface = KColours.neutral0;
+  static const Color surfaceHovered = KColours.neutral200;
+  static const Color surfacePressed = KColours.neutral300;
+  static const Color overlay = KColours.neutral0;
+  static const Color overlayHovered = KColours.neutral200;
+  static const Color overlayPressed = KColours.neutral300;
+  static const Color surfaceRaised = KColours.neutral0;
+  static const Color surfaceRaisedHovered = KColours.neutral200;
+  static const Color surfaceRaisedPressed = KColours.neutral300;
+  static const Color surfaceSunken = KColours.neutral100;
+}
